a89503887ef437598de3e05602799de433b54dd6
juanzink7l <juanzink7l@gmail.com>
2026-08-17 15:36:08 -0300

    Commit final

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template, request
+import os 
+import sys
+import webview
 import yt_dlp
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
 
-@app.route("/")
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, 'static')
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+    app = Flask(__name__)
+
+@app.route('/')
 def index():
-    return render_template("index.html")  # aqui fez a pagina html ser a pagina principal
+    return render_template("index.html")
 
 @app.route("/download", methods=['POST'])
 def download():
-    voltar = '<br><a href="/><b>Voltar</b></a>'
+    voltar = '<br><a href="/"><b>Voltar</b></a>'
     try:
         url = request.form.get('url')
         ydl_opts = {}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download(url)
-            return f"Download concluído{voltar}"
+        return f"Download concluído!{voltar}"
     except Exception as e:
         return f"Erro ao tentar baixar vídeo: {str(e)}.{voltar}"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    window = webview.create_window(
+        title="Download de vídeos",
+        url=app,
+        width=1000,
+        height=700
+    )
+
+#inicia a janela (o pywebview inicia o flask internamente de forma automatica)
+    webview.start()
